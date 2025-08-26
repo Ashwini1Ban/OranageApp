f4f426124bfa7634bb26745542d916b092daf000
Ashwini1Ban <ashwini93@live.com>
2025-08-26 18:33:30 +0530

    second commit done

--- src/test/java/pckgaeO/Login0.java
+++ src/test/java/pckgaeO/Login0.java
@@ -24,7 +24,7 @@ import org.testng.annotations.Test;
 //Navigate to the PIM module, Add Employees, Verify Employees in the Employee List, logout
 
 
-// changes mad by User B 
+// changes mad by User B  ,now A
 public class Login0 {
 
 	WebDriver driver;
